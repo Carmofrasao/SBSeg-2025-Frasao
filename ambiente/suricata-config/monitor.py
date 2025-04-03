0ed1347bbad11e0735128aa0175abc66aeca5baf
root <root@fibra-teste.c3sl.ufpr.br>
2025-04-03 19:30:44 -0300

    suricata config e script feito

--- ambiente/suricata-config/monitor.py
+++ ambiente/suricata-config/monitor.py
@@ -6,12 +6,11 @@ import subprocess
 log_file = '/etc/suricata/eve.json'
 reputation_file = '/etc/suricata/iprep/reputation.list'
 categories = {
-    'BadHosts': (1, 40),
-    'GoodHosts': (2, 100)
-    'Brute Force Attack': (3, 20),
-    'Port Scan': (4, 60),
-    'Malware Distribution': (5, 5),
+    'BadHosts': 1,
+    'GreyHosts': 2,
+    'GoodHosts': 3,
 }
+seen_ips = {}
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_MODIFY(self, event):
@@ -29,31 +28,35 @@ class EventHandler(pyinotify.ProcessEvent):
                         continue
 
 def update_reputation(ip, alert_msg):
-    category_id, reputation_score = categorize_alert(alert_msg)
     if not os.path.exists(reputation_file):
         with open(reputation_file, 'w') as f:
-            f.write(f"{ip},{category_id},{reputation_score}\n")
+            f.write(f"{ip},2,90\n")
     else:
         with open(reputation_file, 'r+') as f:
             lines = f.readlines()
             for i, line in enumerate(lines):
+                # Caso onde o IP ja esta em reputation.list
                 if line.startswith(ip):
-                    lines[i] = f"{ip},{category_id},{reputation_score}\n"
+                    new_reputation = int(line.strip(",")[2])
+                    if new_reputation > 0:
+                        new_reputation -= 10
+                     
+                    new_category = int(line.strip(",")[1])
+                    if new_reputation < 70:
+                        new_category = 2
+                    elif new_reputation < 30:
+                        new_category = 1
+                        
+                    lines[i] = f"{ip},{new_category},{new_reputation}\n"
                     break
             else:
-                lines.append(f"{ip},{category_id},{reputation_score}\n")
+                lines.append(f"{ip},3,90\n")
             f.seek(0)
             f.writelines(lines)
     reload_suricata()
 
-def categorize_alert(alert_msg):
-    for key, (cat_id, score) in categories.items():
-        if key.lower() in alert_msg.lower():
-            return cat_id, score
-    return 0, 50
-
 def reload_suricata():
-    subprocess.run(['kill', '-USR2', str(os.getpid())])
+    subprocess.run(['suricatasc', '-c', 'ruleset-reload-nonblocking'])
     # suricatasc -c "reload-rules"
 
 wm = pyinotify.WatchManager()

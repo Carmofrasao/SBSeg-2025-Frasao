8c8739708571e1a82120499b382a617e4f3fd414
root <root@fibra-teste.c3sl.ufpr.br>
2025-04-06 16:44:18 -0300

    tentando configurar o suricata IPS

--- ambiente/suricata-config/entrypoint.sh
+++ ambiente/suricata-config/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-dnf install -y iptables-legacy iptables-nft libnetfilter_queue nftables
-update-alternatives --set iptables /usr/sbin/iptables-legacy
-#iptables -I FORWARD -j NFQUEUE --queue-num 0
-iptables -I INPUT -j NFQUEUE --queue-num 0
-iptables -I OUTPUT -j NFQUEUE --queue-num 0
+echo 1 > /proc/sys/net/ipv4/ip_forward
+#dnf install -y iptables-legacy iptables-nft libnetfilter_queue nftables
+#update-alternatives --set iptables /usr/sbin/iptables-legacy
+iptables -I FORWARD -j NFQUEUE --queue-num 0
+iptables -t mangle -A PREROUTING -j NFQUEUE --queue-num 0
+#iptables -I INPUT -j NFQUEUE --queue-num 0
+#iptables -I OUTPUT -j NFQUEUE --queue-num 0
 suricata -c /etc/suricata/suricata.yaml -q 0

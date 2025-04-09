86af34c346fb48adcc95e46f95a5de2347309703
root <root@fibra-teste.c3sl.ufpr.br>
2025-04-09 15:46:51 -0300

    Script LUA para probabilidade de DROP de pacotes

---
+++ ambiente/suricata-config/rules/script.lua
@@ -0,0 +1,19 @@
+function init (args)
+    local needs = {}
+    needs["packet"] = tostring(true)
+    return needs
+end
+
+function match(args)
+    a = tostring(args["packet"])
+    if #a > 0 then
+        if math.random() < 0.5 then
+          return 1
+        end
+    end
+
+    return 0
+end
+
+return 0
+

